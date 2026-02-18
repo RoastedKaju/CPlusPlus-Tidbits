6c31408f7c5949a0b44a6417fe522641934b0587
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2026-02-18 06:02:58 +0500

    update concept.h

--- src/concepts.h
+++ src/concepts.h
@@ -6,6 +6,8 @@
 #include <cmath>
 #include <memory>
 #include <vector>
+#include <forward_list>
+#include <unordered_set>
 
 ///
 /// A concept is just a true of false statement, it is like a predicate that returns true or false
@@ -54,13 +56,13 @@ concept AlwaysTrue = true;
 
 // Now use it
 template<typename T> requires AlwaysTrue<T>
-void PrintSomething(T value)
+void printSomething(T value)
 {
 	std::cout << value << std::endl;
 }
 
 // Or avoid all this template crap and simply use auto
-void PrintSomethingAutoVer(AlwaysTrue auto value)
+void printSomethingAutoVer(AlwaysTrue auto value)
 {
 	std::cout << value << std::endl;
 }
@@ -71,7 +73,7 @@ concept IsFloatORInt = std::floating_point<T> || std::integral<T>;
 
 // Use it
 template<typename T> requires IsFloatORInt<T>
-void PrintFloatOrInt(T value)
+void printFloatOrInt(T value)
 {
 	std::cout << value << std::endl;
 }
@@ -81,7 +83,7 @@ template<typename T>
 concept IsDouble = std::same_as<T, double>; // Needs two types
 
 // Use it
-void PrintDoubleAutoVer(IsDouble auto value)
+void printDoubleAutoVer(IsDouble auto value)
 {
 	std::cout << value << std::endl;
 }
@@ -92,7 +94,7 @@ concept SmallDataExpression = sizeof(T) <= 8; // Size is present during compile 
 
 // Use our concepts
 template <typename T> requires SmallDataExpression<T>
-void PrintData(T value)
+void printData(T value)
 {
 	std::cout << "The size of our small data is: " << sizeof(value) << std::endl;
 }
@@ -103,16 +105,47 @@ concept BigDataExpression = !SmallDataExpression<T>; // Just NOT the small data 
 
 // This is kinda like SFINAE where if one template overload fails to do something have another
 template<typename T> requires BigDataExpression<T>
-void PrintData(const T& value)
+void printData(const T& value)
 {
 	std::cout << "The size of our big data is: " << sizeof(value) << std::endl;
 }
 
 // Or you could have done just this
 template<typename T> requires (!SmallDataExpression<T>) // Requires need an expression so enclose it in braces
-void PrintBigData()
+void printBigData()
 {
 	// Print data size
+}
+
+// Now something complex
+// We want to have a template that only compiles for types that have a push back function
+template<typename T>
+concept HasPushback = requires(T container, T::value_type valueType) { // Can also work without value type but you would have to manually add integer in push back below
+	container.push_back(valueType);
+};
+
+template<typename T> requires HasPushback<T>
+void addIntToContainer(T& container, int integer)
+{
+	container.push_back(integer);
+}
+
+// Challenge (write a concpet that checks if the container has a size function)
+template<typename T>
+concept HasSizeFunction = requires(T container) {
+	container.size();
+};
+
+template<typename T> requires HasSizeFunction<T>
+void printContainerSize(T& container)
+{
+	std::cout << "Container Size is: " << container.size() << std::endl;
+}
+
+template<typename T> requires (!HasSizeFunction<T>)
+void printContainerSize(T& container)
+{
+	std::cout << "This one doesn't have a size function so we called this instead." << std::endl;
 }
 
 void conceptsExample()
@@ -125,24 +158,36 @@ void conceptsExample()
 	std::cout << std::boolalpha << isEqual(1u, 5u) << std::endl;
 	std::cout << std::boolalpha << isEqualAutoVer(1.0f, 5.0f) << std::endl; // Auto version
 
-	PrintSomething(10);
-	PrintSomethingAutoVer("Hello World!"); // Since the concept expression result is always true we can put pretty much any argument here
+	printSomething(10);
+	printSomethingAutoVer("Hello World!"); // Since the concept expression result is always true we can put pretty much any argument here
 
 	std::cout << "OR based Concepts" << std::endl;
-	PrintFloatOrInt(10);
-	PrintFloatOrInt(100.0f);
+	printFloatOrInt(10);
+	printFloatOrInt(100.0f);
 	//PrintFloatOrInt("Haris"); // Will not work as the function template only requries floating or integer
 
 	// Custom double concept
-	PrintDoubleAutoVer(1000.0);
+	printDoubleAutoVer(1000.0);
 
-	PrintData(23);
+	printData(23);
 	{
 		std::unique_ptr<int> somePtr; // Should be 8 bytes
-		PrintData(std::move(somePtr));
+		printData(std::move(somePtr));
 	}
-	std::vector<int> someVec = {1, 3, 4, 5, 6};
-	PrintData(someVec); // The size of vector is 32 bytes, doesn't matter how many elements it has
+	std::vector<int> someVec = { 1, 3, 4, 5, 6 };
+	printData(someVec); // The size of vector is 32 bytes, doesn't matter how many elements it has
 	// The big data one will use the 2nd overload with const reference
 
+	// Container example
+	std::vector<int> vec{ 0, 1, 2, 4, 5 };
+	addIntToContainer(vec, 20);
+	std::cout << vec.back() << std::endl;
+
+	std::forward_list<int> list{ 1, 4, 5 };
+	//addIntToContainer(list, 50); // Won't work, foward list doesn't have push_back()
+
+	printContainerSize(vec);
+	printContainerSize(list); // This would call the other overload
+	std::unordered_set<int> someSet{ 10, 2 };
+	printContainerSize(someSet);
 }
